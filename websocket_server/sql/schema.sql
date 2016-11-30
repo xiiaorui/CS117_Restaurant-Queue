57d174a6c2237e59a1f2741ee0953d56199de190
Wesley Millar <wesleykmillar@gmail.com>
2016-11-29 20:59:15 -0800

    Server: Remove restaurants.open from schema

--- websocket_server/sql/schema.sql
+++ websocket_server/sql/schema.sql
@@ -3,13 +3,12 @@ USE `csm117`;
 
 CREATE TABLE IF NOT EXISTS `restaurants` (
   `id` int(10) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `open` tinyint(1) NOT NULL DEFAULT '0'
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 ALTER TABLE `restaurants`
- ADD PRIMARY KEY (`id`), ADD KEY `open` (`open`);
+ ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `restaurants`
 MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
